25809257a89d68f128fbf3752f74971d7c7193d6
REZAUL KARIM RAFI <45324690+rejaul123@users.noreply.github.com>
2024-11-22 21:50:13 +0600

    Delete 1st sem/LAB-1 directory

--- 1st sem/LAB-1/lab1 (2).c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-int  width =5 , height=10 ;
-
-int area =  width*height;
-printf("area of the rectangle: %d\n",area) ;
-
-return 0;
-}
--- 1st sem/LAB-1/lab1 (5).c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-int a = 4;
-int cube = a*a*a;
-printf ("cube of %d: %d\n", a,cube);
-return 0;
-
-}
