3b1a9bdd66bd26acffee6b28cf57d03e9bc698b0
REZAUL KARIM RAFI <45324690+rejaul123@users.noreply.github.com>
2024-11-22 21:45:12 +0600

    Add files via upload

---
+++ 1st sem/LAB-1/lab1 (6).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+
+float celsius = 25;
+
+float fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+printf("Temperature in Fahrenheit: %f\n", fahrenheit);
+
+return 0;
+
+
+
+}
